7af3219252b529c0b15e7d6e63be5f0f2906053b
Adam Kelso <61440423+Kelsoadam@users.noreply.github.com>
2022-04-04 22:47:56 +0100

    Added most of code for navigation panel

---
+++ Mobile App/ProjectApp/app/src/main/java/com/finalproject/app/DrawerBaseActivity.java
@@ -0,0 +1,49 @@
+package com.finalproject.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class DrawerBaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    public void setContentView(View view) {
+        drawerLayout = (DrawerLayout) getLayoutInflater()
+                .inflate(R.layout.activity_drawer_base, null);
+        FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
+        container.addView(view);
+        super.setContentView(drawerLayout);
+
+        Toolbar toolbar = drawerLayout.findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = drawerLayout.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,
+                toolbar,R.string.menu_drawer_open, R.string.menu_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    protected void allocateActivityTitle(String titleString){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle(titleString);
+        }
+    }
+}
